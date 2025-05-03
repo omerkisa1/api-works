24b93520bc5599834211cd7812ee4cab0c136433
omerkisa1 <omereis.5555@gmail.com>
2025-05-03 17:24:56 +0300

    add gitignore and user type examples

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
@@ -24,3 +25,22 @@ async def get_all_users():
 @app.get("/users/current")
 async def get_current_user():
     return {"message": "current user"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id : str):
+    return {"message": f"user {user_id}"}
+
+
+class AccessType(str, Enum):
+    user = "user"
+    admin = "admin"
+    super_admin = "super_admin"
+
+@app.get("/access/{user_type}")
+async def get_user_by_role(user_type: AccessType):
+    if user_type.value == "super_admin":
+        return{"message": "you have full access"}
+    if user_type.value == "admin":
+        return{"message": "you have partial access"}
+    else:
+        return{"message": "normal user"}
